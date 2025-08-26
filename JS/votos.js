71206a75710bf9f73ce309f3512c27db175aa381
Janettth <jalu040803@gmail.com>
2025-08-25 21:26:02 -0600

    Archivos Torneo STEM primer commit

--- JS/votos.js
+++ JS/votos.js
@@ -19,43 +19,43 @@ btn1.addEventListener('click', function() {
     itcj++;
     conta1.textContent = itcj;
     btn1.disabled = true;
-    btn1.textContent = 'votado';
-    btn1.style.background = 'white';
-    btn1.style.color = 'black';
+    btn1.value = "VOTADO" ;
+    btn1.style.background = 'red';
+    btn1.style.color = 'white';
 });
 
 btn2.addEventListener('click', function() {
     tec++;
     conta2.textContent = tec;
     btn2.disabled = true;
-    btn2.textContent = 'votado';
-    btn2.style.background = 'white';
-    btn2.style.color = 'black';
+    btn2.value = 'VOTADO';
+    btn2.style.background = 'red';
+    btn2.style.color = 'white';
 });
 
 btn3.addEventListener('click', function() {
     urn++;
     conta3.textContent = urn;
     btn3.disabled = true;
-    btn3.textContent = 'votado';
-    btn3.style.background = 'white';
-    btn3.style.color = 'black';
+    btn3.value = 'VOTADO';
+    btn3.style.background = 'red';
+    btn3.style.color = 'white';
 });
 
 btn4.addEventListener('click', function() {
     uacj++;
     conta4.textContent = uacj;
     btn4.disabled = true;
-    btn4.textContent = 'votado';
-    btn4.style.background = 'white';
-    btn4.style.color = 'black';
+    btn4.value = 'VOTADO';
+    btn4.style.background = 'red';
+    btn4.style.color = 'white';
 });
 
 btn5.addEventListener('click', function() {
     uach++;
     conta5.textContent = uach;
     btn5.disabled = true;
-    btn5.textContent = 'votado';
-    btn5.style.background = 'white';
-    btn5.style.color = 'black';
+    btn5.value = 'VOTADO';
+    btn5.style.background = 'red';
+    btn5.style.color = 'white';
 });
